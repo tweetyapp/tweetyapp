8d871b7c617436cfb928b192f84066265bbcc269
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-19 01:06:56 +0530

    Added user controller (check layout_links test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Sample::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/contact"
-  
-  get "pages/about"
+  get "users/new"
 
+  root :to => "pages#home"
+  match '/', :to => 'pages#home'
+  match '/contact', :to => 'pages#contact'
+  match '/about', :to => 'pages#about'
+  match '/help', :to => 'pages#help'
+  match '/signup', :to => 'users#new'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,43 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a home page at '/'" do
+  	get '/'
+  	response.should have_selector('title', :content => "Home")
+  end
+
+ it "should have a home page at '/contact'" do
+  	get '/contact'
+ 	response.should have_selector('title', :content => "Contact")
+  end
+
+it "should have a home page at '/about'" do
+ 	get '/about'
+ 	response.should have_selector('title', :content => "About")
+ end
+
+it "should have a home page at '/help'" do
+ 	get '/help'
+ 	response.should have_selector('title', :content => "Help")
+ end
+ it "should have a home page at '/signup'" do
+ 	get '/signup'
+ 	response.should have_selector('title', :content => "Sign up")
+ end
+
+ it "should have right links" do
+ 	visit root_path
+ 	response.should have_selector('title',:content => "Home")
+ 	click_link "About"
+ 	response.should have_selector('title',:content => "About")
+ 	click_link "Contact"
+ 	response.should have_selector('title',:content => "Contact")
+ 	click_link "Home"
+ 	response.should have_selector('title',:content => "Home")
+ 	click_link "Sign up now!"
+ 	response.should have_selector('title',:content => "Sign up")
+ 	response.should have_selector('a[href="/"]>img')
+ end
+
+end
