eeb8ec712dee0f0effc97916e2babcb42b80b4d8
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-18 19:13:38 +0530

    Added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+
+  # title for page
+  
+  def title
+    base_title ="RoR Tutorials Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
