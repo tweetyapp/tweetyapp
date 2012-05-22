fb9869d3cbd1f1b0116bb4ae064240f4260c5f99
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-23 01:07:26 +0530

    added micropost validations

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,4 +3,8 @@ class Micropost < ActiveRecord::Base
 	attr_accessible :content
 
 	default_scope :order => 'microposts.created_at DESC'
+
+	validates :content, :presence => true,
+						:length { :maximum => 140}
+	validates :user_id, :presence => true
 end
