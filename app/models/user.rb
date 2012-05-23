e286bd466a12cab38181a3efc6bf93d6904300d0
chronus <chronus@chronus-Invalid-entry-length-0-DMI-table-is-broken-Stop.(none)>
2012-05-23 20:36:12 +0530

    user relationship following follwers

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,20 @@
 class User < ActiveRecord::Base
 	has_many :microposts, :dependent => :destroy
+	has_many :relationships, :dependent => :destroy,
+							 :foreign_key => "follower_id"
+
+	has_many :following, :through => :relationships, :source => :followed
+
+	has_many :reverse_relationships , :dependent => :destroy,
+										:foreign_key => "followed_id",
+										:class_name => "Relationship"
+	has_many :followers, :through => :reverse_relationships,
+										:source => :follower
+
 	attr_accessor :password
+
 	attr_accessible :name,:email, :password, :password_confirmation
+
 	email_regex =/\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :name, :presence => true,
 						:length => { :maximum => 50}
@@ -21,6 +34,17 @@ class User < ActiveRecord::Base
 		Micropost.where("user_id = ?",id)
 	end
 
+	def following?(followed)
+		self.relationships.find_by_followed_id(followed)
+	end
+
+	def follow!(followed)
+		self.relationships.create!(:followed_id => followed.id)
+	end
+
+	def unfollow!(followed)
+		self.relationships.find_by_followed_id(followed).destroy
+	end
 	class << self
 		def authenticate(email,submitted_password)
 			user = find_by_email(email)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120522184527) do
+ActiveRecord::Schema.define(:version => 20120523131717) do
 
   create_table "microposts", :force => true do |t|
     t.string   "content"
@@ -21,6 +21,16 @@ ActiveRecord::Schema.define(:version => 20120522184527) do
 
   add_index "microposts", ["user_id"], :name => "index_microposts_on_user_id"
 
+  create_table "relationships", :force => true do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "relationships", ["followed_id"], :name => "index_relationships_on_followed_id"
+  add_index "relationships", ["follower_id"], :name => "index_relationships_on_follower_id"
+
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
@@ -29,6 +39,7 @@ ActiveRecord::Schema.define(:version => 20120522184527) do
     t.string   "encrypted_password"
     t.string   "salt"
     t.boolean  "admin",              :default => false
+    t.string   "url",                :default => "id"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
